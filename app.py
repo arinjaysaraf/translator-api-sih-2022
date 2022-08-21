f92a5700cc10f828a7840146db15bc00503733be
Arinjay Saraf <arinjaysaraf@gmail.com>
2022-08-21 17:40:26 +0530

    returning dict

--- app.py
+++ app.py
@@ -12,14 +12,31 @@ app.add_middleware(
     allow_methods=[""],
     allow_headers=[""],
 )
-class transalationClass(BaseModel): 
+
+class translationClass(BaseModel): 
     sentence:str 
     toLang:str
     fromLang:str 
 
+toLangs = ['en', 'gu', 'hi', 'kn', 'kok', 'mr', 'pa', 'sa', 'ta', 'te']
+translatedLangs = {}
+
 
 @app.post("/")
-async def getTrans(translation:transalationClass):
-    translator = Translator()
-    translated = (translator.translate(translation.sentence, dest= translation.toLang,src = translation.fromLang))
-    return (translated.text)
+async def getTrans(translation:translationClass):
+    for i in toLangs:
+        translator = Translator()
+        translated = (translator.translate(translation.sentence, dest= i,src = translation.fromLang))
+        translatedLangs[i] = translated.text
+    return translatedLangs
+
+#   \n en English(India)
+#   \n gu-IN Gujarati(India)
+#   \n hi-IN Hindi(India) 
+#   \n kn-IN Kannada(India) 
+#   \n kok-IN Konkani(India) 
+#   \n mr-IN Marathi(India) 
+#   \n pa-IN Punjabi(India) 
+#   \n sa-IN Sanskrit(India) 
+#   \n ta-IN Tamil(India) 
+#   \n te-IN Telugu(India)
